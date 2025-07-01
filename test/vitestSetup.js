f623ade46a5564c9fd50f3f603cfbcfd5bd2f9b3
Alex Candelas <axcandelas@gmail.com>
2025-06-30 23:04:15 -0600

    Make string replacement for test comparisons more controlled

--- test/vitestSetup.js
+++ test/vitestSetup.js
@@ -1,8 +1,16 @@
 import { expect } from 'vitest';
 
 function replaceCss(str) {
-    return str.replace(/\s/g, '')
-        .replaceAll(';}', '}');
+    return str.replaceAll(/\s+/g, ' ')  // Multiple spaces and linebreaks to a single space
+        .replace(/^\s/, '')             // Remove leading space
+        .replace(/\s$/, '')             // Remove trailing space
+        .replace(/\s?{\s?/g, '{')       // ' { ' => '{'
+        .replaceAll('( ', '(')          // '( ' => '('
+        .replace(/\s([})])/g, '$1')     // ' }' => '}' and ' )' => ')'
+        .replace(/([:;])\s/g, '$1')     // ': ' => ':' and '; ' => ';'
+        .replace(/\s?\/\s?/g, '/')      // ' / ' => '/'
+        .replaceAll(';}', '}')          // ';}' => '}'
+        .replace(/(\.\\3\d)\s/g, '$1'); // Remove space in classes that start with a digit: '.\36 00px' => '.\3600px
 }
 
 expect.extend({
